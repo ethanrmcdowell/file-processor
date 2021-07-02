ea4eab4946d1bae462a09f4f9bb5df596e6f85c0
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2021-07-01 22:06:32 -0400

    notes for tomorrow

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const inquirer = require('inquirer');
 const crypto = require('crypto');
 const fs = require('fs');
 
+// TOMORROW:
+// create switch statement for file sigs
+// figure out what to do with directories
+// implement inquirer for command line UI
+// md5 hash of pdf and jpg files
+// combine into object -> csv file
+
 const init = () => {
   fs.readdirSync('./Target/').forEach(async (file, error) => {
     const boolDir = await fs.lstatSync('./Target/' + file).isDirectory();
@@ -12,10 +19,14 @@ const init = () => {
         .toString('hex')
         .slice(0, 9);
       if (fileHex === '255044462') {
-        console.log('**PDF: ' + file);
+        handlePdf(file);
       }
     }
   });
+};
+
+const handlePdf = file => {
+  console.log(file);
 };
 
 // const init = async () => {
