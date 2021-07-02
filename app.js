ecc2c998a935e3df5099cc12d97db976844e551a
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2021-07-02 18:03:17 -0400

    added md5 hash functionality, currently will log md5 hash of pdf files in "./Target/" directory

--- app.js
+++ app.js
@@ -2,37 +2,61 @@ const inquirer = require('inquirer');
 const crypto = require('crypto');
 const fs = require('fs');
 
-// TOMORROW:
-// create switch statement for file sigs
-// figure out what to do with directories
+// ----------------------------------------------
+//           TO DO & NOTES:
+// ----------------------------------------------
+// figure out what to do with directories:
+// UPDATE: so far have directoryList array where each
+// directory gets placed - will create further logic later.
+// ----------------------------------------------
 // implement inquirer for command line UI
+// ----------------------------------------------
 // md5 hash of pdf and jpg files
+// ----------------------------------------------
 // combine into object -> csv file
+// ----------------------------------------------
+
+const directoryList = [];
+const md5sum = crypto.createHash('md5');
 
 const init = () => {
-  fs.readdirSync('./Target/').forEach(async (file, error) => {
-    const boolDir = await fs.lstatSync('./Target/' + file).isDirectory();
-    const boolFile = await fs.lstatSync('./Target/' + file).isFile();
-    if (boolFile) {
-      const fileHex = await fs
-        .readFileSync('./Target/' + file)
-        .toString('hex')
-        .slice(0, 9);
-      if (fileHex === '255044462') {
-        handlePdf(file);
+  try {
+    fs.readdirSync('./Target/').forEach((file, error) => {
+      const boolDir = fs.lstatSync('./Target/' + file).isDirectory();
+      const boolFile = fs.lstatSync('./Target/' + file).isFile();
+      if (boolDir) {
+        handleDir(file);
+      } else if (boolFile) {
+        const fileHex = fs
+          .readFileSync('./Target/' + file)
+          .toString('hex')
+          .slice(0, 9);
+        if (fileHex === '255044462') handlePdf(file);
+        else if (fileHex.slice(0, 4) === 'ffd8') handleJpg(file);
       }
-    }
-  });
+    });
+  } catch (err) {
+    console.log(err);
+  } finally {
+    // console.log('---- FIN ----');
+    // console.log('DIRECTORIES: ' + directoryList);
+  }
 };
 
-const handlePdf = file => {
-  console.log(file);
+const handlePdf = async file => {
+  const thisFile = await fs.readFileSync('./Target/' + file);
+  const hash = crypto.createHash('md5').update(thisFile).digest('hex');
+  console.log(file + ' / MD5 Hash: ' + hash);
+  // console.log('PDF: ' + file);
 };
 
-// const init = async () => {
-//   console.log(
-// await fs.readFileSync('./Letter.pdf').toString('hex').slice(0, 9)
-//   );
-// };
+const handleJpg = file => {
+  // console.log('JPG: ' + file);
+};
+
+const handleDir = file => {
+  // directoryList.push(file);
+  // console.log('DIR: ' + file + '/');
+};
 
 init();
