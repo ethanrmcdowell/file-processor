8846de0586fd4f9537d4e2c2dfb4c234ff08bd37
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2021-07-03 21:17:10 -0400

    Working on data flow, planning restructure

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const directoryList = [];
 const fileList = [];
 // subdirFlag should be set false by defult, set true for testing
 let subdirFlag = true;
-let targetPath = '/Users/ethanmcdowell/Projects/file-scanner/Target/';
+let targetPath = '/Users/ethanmcdowell/Projects/file-scanner/Target' + '/';
 let originalTarget = '';
 
 const initPrompt = {
@@ -25,8 +25,14 @@ const initPrompt = {
   message: 'Enter required information:',
 };
 
-const init = () => {
-  userInput();
+const init = async () => {
+  try {
+    await userInput();
+  } catch (err) {
+    console.log(err);
+  } finally {
+    console.log(fileList);
+  }
 };
 
 const userInput = async () => {
@@ -39,7 +45,7 @@ const userInput = async () => {
   //     subdirFlag = true;
   //   }
   // });
-  scanFiles();
+  await scanFiles();
 };
 
 const scanFiles = async () => {
@@ -48,6 +54,7 @@ const scanFiles = async () => {
       const boolDir = fs.lstatSync(targetPath + file).isDirectory();
       const boolFile = fs.lstatSync(targetPath + file).isFile();
       if (boolDir) {
+        console.log('Directory: ' + file + '/');
         handleDir(file);
       } else if (boolFile) {
         const fileHex = fs
@@ -62,11 +69,18 @@ const scanFiles = async () => {
     console.log(err);
   } finally {
     if (subdirFlag) {
-      console.log('SUBDIRECTORY FLAG TURNED ON, SCANNING SUBDIRECTORIES');
-      handleSubdir();
+      try {
+        console.log('SCANNING SUBDIRECTORIES:');
+        await handleSubdir();
+      } catch (err) {
+        console.log(err);
+      } finally {
+        console.log('CREATING YOUR FILE, PLEASE BE PATIENT :)');
+        await createCsv();
+      }
     } else if (!subdirFlag) {
       console.log('CREATING YOUR FILE, PLEASE BE PATIENT :)');
-      createCsv();
+      await createCsv();
     }
   }
 };
@@ -76,17 +90,19 @@ const handlePdf = async file => {
   const hash = crypto.createHash('md5').update(thisFile).digest('hex');
   const newFileInfo = new FileInfo(targetPath + file, '.pdf', hash);
   fileList.push(newFileInfo);
+  // console.log(fileList);
 };
 
 const handleJpg = async file => {
   const thisFile = await fs.readFileSync(targetPath + file);
-  const hash = crypto.createHash('md5').update(thisFile).digest('hex');
+  const hash = await crypto.createHash('md5').update(thisFile).digest('hex');
   const newFileInfo = new FileInfo(targetPath + file, '.jpg', hash);
   fileList.push(newFileInfo);
+  // console.log(fileList);
 };
 
 const createCsv = () => {
-  console.log('CREATE CSV FUNCTION ~~');
+  console.log('INSIDE CREATE CSV FUNCTION');
   // for (let i = 0; i < fileList.length; i++) {
   //   console.log(
   //     fileList[i].path + ',' + fileList[i].type + ',' + fileList[i].md5
@@ -98,14 +114,15 @@ const handleDir = file => {
   directoryList.push(file);
 };
 
-const handleSubdir = () => {
+const handleSubdir = async () => {
   if (directoryList.length === 0) {
     console.log('THERE ARE NO SUBDIRECTORIES!');
   } else {
     try {
       originalTarget = targetPath;
+      console.log(originalTarget);
       directoryList.forEach(async directory => {
-        targetPath = originalTarget + directory;
+        targetPath = originalTarget + directory + '/';
         await subdirScan();
       });
     } catch (err) {
@@ -114,8 +131,24 @@ const handleSubdir = () => {
   }
 };
 
-const subdirScan = () => {
-  console.log(targetPath);
+const subdirScan = async () => {
+  await fs.readdirSync(targetPath).forEach(async (file, error) => {
+    const boolDir = fs.lstatSync(targetPath + file).isDirectory();
+    const boolFile = fs.lstatSync(targetPath + file).isFile();
+    if (boolDir) {
+      handleDir(file);
+    } else if (boolFile) {
+      const fileHex = fs
+        .readFileSync(targetPath + '/' + file)
+        .toString('hex')
+        .slice(0, 9);
+      if (fileHex === '255044462') {
+        await handlePdf(file);
+      } else if (fileHex.slice(0, 4) === 'ffd8') {
+        await handleJpg(file);
+      }
+    }
+  });
 };
 
 init();
