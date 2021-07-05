a8619aad7a758fd507ae490b406f0c96f26f62fa
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2021-07-05 15:35:45 -0400

    App now fully-functional - will take properly formatted user input, scan all files and/or subfolder's files depending on input, and spit out a csv file with required/requested file information

--- app.js
+++ app.js
@@ -12,28 +12,40 @@ const fs = require('fs');
 
 const methods = require('./methods/methods.js');
 
-let subdirFlag = true;
-let targetDir = __dirname + '/Target/';
+let subdirFlag = false;
+let targetDir = '';
 let outputPath = '';
 let csvText = '';
+let csvCount = 0;
 
 const init = async () => {
   try {
-    // await userInput();
+    console.log(__dirname);
+    await userInput();
     await methods.scanTarget(targetDir, subdirFlag);
   } catch (err) {
     console.log(err);
   } finally {
     try {
       methods.fileList.forEach(file => {
-        csvText += file.path + ', ' + file.type + ', ' + file.md5 + '\r\n';
+        csvCount++;
+        csvText +=
+          csvCount +
+          ', ' +
+          file.path +
+          ', ' +
+          file.type +
+          ', ' +
+          file.md5 +
+          '\r\n';
       });
     } finally {
-      fs.writeFile(targetDir + 'fileList.csv', csvText, 'utf8', err => {
+      fs.writeFile(outputPath, csvText, 'utf8', err => {
         if (err) {
           console.log(err);
         } else {
-          console.log('CSV file written to: ' + targetDir + 'fileList.csv');
+          console.log('CSV file written to: ' + outputPath);
+          console.log('Output Length:' + methods.fileList.length);
         }
       });
     }
@@ -43,16 +55,16 @@ const init = async () => {
 const userInput = async () => {
   await inquirer.prompt(methods.initPrompt).then(async initRes => {
     const userRes = initRes.target;
-    const inputLength = userRes.split(/[ ,]+/).length;
-    targetDir = userRes.split(/[ ,]+/)[0] + '/';
-    outputPath = userRes.split(/[ ,]+/)[1];
-    if (inputLength === 3) {
+    const inputArray = userRes.split(' ');
+    targetDir = inputArray[0] + '/';
+    outputPath = inputArray[1];
+    if (inputArray[2] === 'r') {
+      console.log('Subdirectory search is turned on!');
       subdirFlag = true;
     }
   });
-  console.log('Target Directory: ' + targetDir);
-  console.log('Output Path: ' + outputPath);
-  console.log('Scan subdirectories? ' + subdirFlag);
 };
 
 init();
+
+// /Users/ethanmcdowell/Projects/file-scanner/Target /Users/ethanmcdowell/Projects/file-scanner/Output/output.csv r
